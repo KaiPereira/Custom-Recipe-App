23c9334e8efd62fa35b98ca04a4d603f893dd210
KaiPereira <88850028+KaiPereira@users.noreply.github.com>
2022-12-16 16:43:23 +0000

    backup

---
+++ my-app/pages/index.tsx
@@ -0,0 +1,44 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import React from "react"
+import { Configuration, OpenAIApi } from "openai";
+const config = new Configuration({ apiKey: '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' });
+const openai = new OpenAIApi(config);
+
+export default function Home() {
+  const [content, changeContent] = React.useState()
+  const [input, changeInput] = React.useState()
+
+  const prompt = `
+    Generate a list of recipes using the ingredients and the format below:
+
+    ingredients: ${input}.
+
+    Recipe name, ingredients using precise mesurements, instructions.
+    `;
+
+    const generate = async (prompt: any) => {
+        
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            max_tokens: 1443
+        });
+
+        changeContent(completion.data.choices[0].text)
+    }
+
+    function changeInputFunction(e) {
+      changeInput(e.target.value)
+    }
+
+
+  return (
+    <div className="main">
+      <p>Ingredients</p>
+      <input type="text" onChange={changeInputFunction} />
+      <p>{content}</p>
+      <button onClick={() => generate(prompt)}>Generate a Recipe</button>
+    </div>
+  )
+}
